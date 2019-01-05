a1a68ef151135293dc92d7e19a8d39e22effaf5b
Zino Kader <zino@zinokader.se>
2019-01-05 12:48:15 +0100

    Booking site formatting

--- booking.py
+++ booking.py
@@ -1,24 +1,20 @@
 from datetime import *
 import re
 
-default_datetime_format = '%Y-%m-%d %H:%M:%S'
+default_date_format = "%Y-%m-%d"
+default_time_format = '%H:%M:%S'
 any_regex = "(.*)"
 start_time_beginning = "start="
 end_time_beginning = "end="
-string_end = "&"
+start_time_string_end = "&end"
+end_time_string_end = "&sportIds"
 
 
 class TimeSlot:
     def __init__(self, booking_url):
         self.booking_url = booking_url
-        self.start_time = float(re.search(start_time_beginning + any_regex + string_end, booking_url).group(1)[0])
-        self.end_time = float(re.search(end_time_beginning + any_regex + string_end, booking_url).group(1)[0])
-
-    def get_booking_url(self):
-        return self.booking_url
-
-    def get_start_time(self):
-        return datetime.utcfromtimestamp(self.start_time).strftime(default_datetime_format)
-
-    def get_end_time(self):
-        return datetime.utcfromtimestamp(self.end_time).strftime(default_datetime_format)
+        start_time_seconds = int(re.search(start_time_beginning + any_regex + start_time_string_end, booking_url).group(1)) / 1000
+        end_time_seconds = int(re.search(end_time_beginning + any_regex + end_time_string_end, booking_url).group(1)) / 1000
+        self.date = datetime.utcfromtimestamp(start_time_seconds).strftime(default_date_format)
+        self.start_time = datetime.utcfromtimestamp(start_time_seconds).strftime(default_time_format)
+        self.end_time = datetime.utcfromtimestamp(end_time_seconds).strftime(default_time_format)
