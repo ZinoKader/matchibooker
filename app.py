794720756c4dfa8539f3a490f66273376c3cfdea
Zino Kader <zino@zinokader.se>
2019-01-05 12:16:49 +0100

    Now correctly fetches available time slots without 500 error

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import *
-from bs4 import *
-from datetime import *
 from booking import *
-import http.cookiejar as cookielib
 import mechanicalsoup
-
 
 app = Flask(__name__)
 
@@ -14,7 +10,8 @@ default_user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKi
                      "Chrome/71.0.3578.98 Safari/537.36 "
 
 default_date_format = "%Y-%m-%d"
-default_schedule_url = "https://www.matchi.se/book/schedule?name=spangatbk&sport=1&indoor=true"
+default_base_url = "https://www.matchi.se"
+default_schedule_url = "https://www.matchi.se/book/schedule?name=spangatbk&sport=1&indoor=true&facilityId=55&wl="
 available_time_class = "courtLinks"
 
 
@@ -39,16 +36,12 @@ def booking():
     browser.session.headers["referer"] = browser.get_url()
     browser.session.headers["user-agent"] = default_user_agent
 
-    #browser.open("https://www.whoishostingthis.com/tools/user-agent/")
-    #print(browser.get_current_page().text)
-
     booking_slots = get_booking_slots()
 
     return render_template("booking.html", booking_slots=booking_slots)
 
 
 def get_booking_slots():
-
     days_to_get = []
     for day_offset in range(0, 5):
         day = datetime.today() + timedelta(days=day_offset)
@@ -57,11 +50,9 @@ def get_booking_slots():
     booking_slots = []
     for day in days_to_get:
         browser.open(default_schedule_url + "&date=" + day)
-        #print(browser.get_current_page().text)
-        #print(browser.get_current_page().find("a", {"class": available_time_class}))
         timeslot_elements = browser.get_current_page().findAll("a", {"class": available_time_class})
         for timeslot_element in timeslot_elements:
-            timeslot_url = timeslot_element["href"]
+            timeslot_url = default_base_url + timeslot_element["href"]
             timeslot = TimeSlot(timeslot_url)
             booking_slots.append(timeslot)
 
@@ -70,13 +61,9 @@ def get_booking_slots():
 
 @app.route("/book")
 def book():
-
     booking_page = request.form["booking_page"]
-
     browser.open(booking_page)
     booking_page = browser.get_current_page()
-
-    print(booking_page.text)
     return booking_page.prettify()
 
 
--- booking.py
+++ booking.py
@@ -1,6 +1,8 @@
 from datetime import *
+import re
 
 default_datetime_format = '%Y-%m-%d %H:%M:%S'
+any_regex = "(.*)"
 start_time_beginning = "start="
 end_time_beginning = "end="
 string_end = "&"
@@ -9,10 +11,8 @@ string_end = "&"
 class TimeSlot:
     def __init__(self, booking_url):
         self.booking_url = booking_url
-        self.start_time = booking_url[booking_url.find(start_time_beginning) +
-                                      len(start_time_beginning): booking_url.rfind(string_end)]
-        self.end_time = booking_url[booking_url.find(end_time_beginning) +
-                                    len(end_time_beginning): booking_url.rfind(string_end)]
+        self.start_time = float(re.search(start_time_beginning + any_regex + string_end, booking_url).group(1)[0])
+        self.end_time = float(re.search(end_time_beginning + any_regex + string_end, booking_url).group(1)[0])
 
     def get_booking_url(self):
         return self.booking_url
